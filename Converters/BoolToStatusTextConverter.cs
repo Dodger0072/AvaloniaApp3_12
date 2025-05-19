94c908a834d7186d2d39c8dea2f1bd522015446a
mrdie <mr.Diego.77@mail.ru>
2025-05-20 00:23:37 +0300

    Init commit

---
+++ Converters/BoolToStatusTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace DairyFarm.Converters
+{
+    public class BoolToStatusTextConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool isBusy)
+            {
+                string role = parameter as string ?? "Работник";
+                return isBusy ? $"{role}: Занят" : $"{role}: Свободен";
+            }
+            return "Статус: Неизвестен";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
